cba9b8dbd3c48a0028721d227a658acd38a515b6
Stian Mogen <stianfmo@stud.ntnu.no>
2021-03-14 13:44:03 +0100

    update calculator functionality
    still buggy

--- oblig4/vueapp/main.js
+++ oblig4/vueapp/main.js
@@ -1,33 +1,62 @@
 const app = Vue.createApp({
     data() {
         return {
-            previous: null,
-            current: null,
-            operator: null,
-            answer: 'answer here',
+            previous: "",
+            current: "",
+            operator: "",
+            show: "",
             log: [],
             product: 'Kalkulator',
             brand: 'Team 17',
             description: 'Mogen, Schiøll-Johansen, Steira',
+            decimal: false
         }
     },
     methods: {
-        setCurrent(event){
-            
+        setCurrent(value){
+            selectedNumber = value.toString();
+            this.current += selectedNumber;
+            this.show = this.current; 
         },
-        operate(o){
-            previous = current; 
-            operator = o;
-            current = null; 
+        operate(value){
+            selectedOperator = value;
+            
+            if (this.current && this.previous)
+                this.current = this.calculate();
+            
+            if (selectedOperator === '=') this.operator = "";
+            else this.operator = selectedOperator;
+            this.previous = this.current;
+            this.show = this.current;
+            this.current = ""; 
+
+            if (decimal) decimal = false; 
         },
         calculate(){
-            if (operator == '+') previous += current;
-            if (operator == '-') previous -= current;
-            if (operator == '*') previous *= current;
-            if (operator == '/') previous /= current; 
+            if (this.current === "")
+                return;
+
+            previous = parseInt(this.previous);
+            current = parseInt(this.current);
+
+            sum = "";
+            if (this.operator == '+') sum = previous += current;
+            if (this.operator == '-') sum = previous -= current;
+            if (this.operator == '*') sum = previous *= current;
+            if (this.operator == '/') sum = previous /= current; 
+            //if (this.operator == '=') sum = previous; 
+            
+            // TODO: add to log
+            return sum;
         },
         addToLog(variantImage){
             this.image = variantImage; 
+        },
+
+        clear(){
+            this.current = "";
+            this.previous = "";
+            this.show = ""; 
         }
     },
     computed: {
